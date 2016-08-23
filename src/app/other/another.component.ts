44eb3ce0106776cb2fd64dc952da690444313c83
Daniel Park <menoelpis@gmail.com>
2016-08-23 09:17:35 +0900

    Added Another Component

---
+++ src/app/other/another.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-another',
+  template: `
+    <p>
+      another Works!
+    </p>
+  `
+})
+export class AnotherComponent {
+}
--- src/app/other/index.ts
+++ src/app/other/index.ts
@@ -1,1 +1,2 @@
-export * from './other.component';
+export { OtherComponent } from './other.component';
+export { AnotherComponent } from './another.component';
