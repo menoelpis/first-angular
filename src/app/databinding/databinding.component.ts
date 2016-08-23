8399ef94eb09ccc259cff3fd42d36f56e0621f75
Daniel Park <menoelpis@gmail.com>
2016-08-23 17:39:41 +0900

    Added Custom Property Binding

--- src/app/databinding/databinding.component.ts
+++ src/app/databinding/databinding.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { PropertyBindingComponent } from "./property-binding.component";
 
 @Component({
   moduleId: module.id,
   selector: 'app-databinding',
   templateUrl: 'databinding.component.html',
-  styleUrls: ['databinding.component.css']
+  styleUrls: ['databinding.component.css'],
+  directives: [PropertyBindingComponent]
 })
 export class DatabindingComponent {
   stringInterpolation = 'This is string interpolation!';
